79fbf9a9a0ce5a7dd044d92a0497f8259b416baa
Chan Lee <chanethanlee@gmail.com>
2018-10-24 23:28:09 -0700

    add authhttp for protecting api.

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,3 +1,4 @@
+import { AuthHttp } from 'angular2-jwt';
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
@@ -5,10 +6,12 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class OrderService {
 
-  constructor(private http: Http) {
+  constructor(private authHttp: AuthHttp, private http: Http) {
   }
 
   getOrders() { 
+    // it will automatically set headers.
+
     return this.http.get('/api/orders')
       .map(response => response.json());
   }
